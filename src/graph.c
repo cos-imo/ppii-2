d27867156374c611d2139478b473f995488c0856
DALIGAUD Kieran <kieran.daligaud@telecomnancy.eu>
2023-04-18 00:04:12 +0200

    Fichiers main et graph

--- src/graph.c
+++ src/graph.c
@@ -20,7 +20,6 @@ int get_nb_vertices(Graph *graph) {
 }
 
 
-
 // Function to return the distance between two electric stations
 float distance_between(Graph graph, int id_station1, int id_station2){
     Link *current_link = graph.link_list;
@@ -43,6 +42,101 @@ float distance_between(Graph graph, int id_station1, int id_station2){
 }
 
 
+
+// Function to create an empty trip
+Trip* createTrip(int id_station){
+    Trip *trip = (Trip*)malloc(sizeof(Trip));
+    trip->id_borne = id_station;
+    trip->next = NULL;
+
+    return trip;
+}
+
+
+// Function to tell if a trip is empty
+Bool isEmpty(Trip *trip){
+    if (trip == NULL){
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    };
+}
+
+
+// Function to add an electric station to a trip
+void addStop(Trip *trip, int id_station){
+    Trip *new = (Trip*)malloc(sizeof(Trip));
+    new->id_borne = id_station;
+    new->next = NULL;
+
+    Trip *current = trip;
+    while (current->next != NULL){
+        current = current->next;
+    }
+    
+    current->next = new;
+}
+
+
+// Function to remove last step of a trip
+void removeStop(Trip *trip){
+    Trip *predecessor = trip;
+    Trip *current = trip->next;
+    while (current->next != NULL){
+        predecessor = predecessor->next;
+        current = current->next;
+    }
+
+    // predecessor.next = current AND current.next = NULL
+    predecessor->next = NULL;
+    free(current);
+}
+
+
+// Function returning the number of stops in a trip
+int get_nb_stops(Trip *trip){
+    int res = 0;
+    Trip *current = trip;
+    while (current != NULL){
+        current = current->next;
+        res++;
+    }
+
+    return res;
+}
+
+
+// Function to show all vertices of a trip
+void showTrip(Trip *trip){
+    printf("Trip: ");
+    Trip *current = trip;
+    while (current != NULL){
+        printf("%d", current->id_borne);
+        current = current->next;
+        if (current != NULL){
+            printf("-");
+        }
+    }
+    printf("\n");
+}
+
+
+// Function to free memory of a trip
+void freeTrip(Trip *trip){
+    Trip *current = trip;
+    Trip *to_come = trip->next;
+    while (to_come != NULL){
+        current->next = NULL;
+        free(current);
+        current = to_come;
+        to_come = to_come->next;
+    }
+    free(current); // Freeing last element
+}
+
+
+// Function to compute the total distance of a trip
 float distance_trip(Graph *graph, Trip *trip){
     int d = 0;
     
@@ -65,27 +159,18 @@ float distance_trip(Graph *graph, Trip *trip){
 }
 
 
-// Function to add an electric station to a trip
-void addVertex(Trip *trip, int id_borne){
-    Trip *current_vertex = trip->next;
-    while (current_vertex != NULL){
-        current_vertex = current_vertex->next;
-    }
-
-    // Building new vertex
-    Trip *new_station = (Trip*)malloc(sizeof(Trip));
-    new_station->id_borne = id_borne;
-    
-    // Linking it to the rest of the trip
-    current_vertex->next = new_station;
-}
-
-
 // Function to compute the distance between 2 vertices
 int distance(int idBorne1, int idBorne2){
     // A toi Thomas
+    return idBorne1 + idBorne2; // Pour pas avoir d'erreur...
 }
 
 
 // Function computing Dijkstra Algorithm
 Trip dijkstra(Graph graph);
+
+
+
+int main(){
+    return EXIT_SUCCESS;
+}
--- src/graph.h
+++ src/graph.h
@@ -33,7 +33,12 @@ typedef struct Trip{
 // Graph Functions
 int get_nb_vertices(Graph *graph);
 float distance_between(Graph graph, int id_borne1, int id_borne2);
-float distance_trip(Graph *graph, Trip *trip);
+Bool isEmpty(Trip *trip);
 void addVertex(Trip *trip, int id_borne);
+void removeVertex(Trip *trip);
+int get_nb_stops(Trip *trip);
+void showTrip(Trip *trip);
+void freeTrip(Trip *trip);
+float distance_trip(Graph *graph, Trip *trip);
 int distance(int idBorne1, int idBorne2);
 Trip dijkstra(Graph graph);
