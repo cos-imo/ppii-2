e54ec80ee52671c757675b72d0162afd4cfb7f8a
eleve <Kieran.Daligaud@telecomnancy.eu>
2023-05-11 13:29:19 +0200

    Test graph

--- tests/test_graph.c
+++ tests/test_graph.c
@@ -2,9 +2,39 @@
 #include <stdlib.h>
 #include "assert.h"
 
-#include "test_graph_trip.h"
+#include "test_graph.h"
 
 
+int testGraph(){
+    // Creating empty graph
+    Graph *graph = createGraph();
+    showGraph(graph);
+
+    // Adding vertices and making the graph complete at the same time
+    addVertexComplete(graph, 0);
+    showGraph(graph);
+
+    addVertexComplete(graph, 2);
+    showGraph(graph);
+
+    addVertexComplete(graph, 9);
+    showGraph(graph);
+    
+    addVertexComplete(graph, 4);
+    showGraph(graph);
+
+    addVertexComplete(graph, 4);
+    showGraph(graph);
+
+    addVertexComplete(graph, 12);
+    showGraph(graph);
+
+    // Freeing the entire graph
+    freeGraph(graph);
+
+
+    return EXIT_SUCCESS;
+}
 
 
 
