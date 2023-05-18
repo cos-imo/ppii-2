b9dde543b73877f03b39a235ded126e03f79ddc2
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-05-18 22:43:51 +0200

    Ajout des fichiers d'initialisation des tableaux de struct

---
+++ src/init_bornes.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "borne.h"
+
+BorneElectrique* *tab;
+int max_id;
+
+void destroy(){
+    for (int i = max_id; i>0; i--){
+        free(tab[i]);
+    }
+    free(tab);
+}
+
+int
+main(
+	int argc,
+	char* *argv)
+{
+
+	 FILE* fp = fopen("../assets/bornes_parsed.csv", "r");
+
+	int max_id = 0;
+
+    if (!fp)
+        printf("Importation du fichier data-bornes impossible\n Initialisation iterompue\n\n");
+
+    else {
+        printf("Ouverture du fichier data-bornes réussie\n Début de l'importation...\n");
+        char buffer[1024];
+
+        int row = 0;
+        int column = 0;
+
+				tab = (BorneElectrique* *)malloc(sizeof(BorneElectrique));
+
+        int current_id = 0;
+
+        while (fgets(buffer,
+                     1024, fp)) {
+            column = 0;
+            row++;
+
+            if (row == 1)
+                continue;
+
+            char* value = strtok(buffer, ",");
+
+            while (value) {
+        	            BorneElectrique *Borne = (BorneElectrique *)malloc(sizeof(BorneElectrique));
+											assert(Borne!=NULL);
+
+											char *end_ptr1;
+        	            char *end_ptr2;
+
+											tab = (BorneElectrique* *)realloc(tab,sizeof(BorneElectrique *)*max_id);
+											assert(tab!=NULL);
+											tab[max_id]=Borne;
+
+                switch (column){
+        			case 0:
+        			    Borne->id = current_id;
+        			    current_id ++;
+        			    Borne->nbVoitures = 0;
+        			    Borne->nbVoituresMax = 100;
+                        Borne->latitude = strtol(value,&end_ptr1,10);
+			    	break;
+
+			    	case 1:
+                        Borne->longitude = strtol(value,&end_ptr2, 10);
+			    	break;
+		}
+                value = strtok(NULL, ",");
+                column++;
+            }
+        }
+        printf("Importation terminée\n");
+        fclose(fp);
+    }
+
+    	return 0;
+}
+
---
+++ src/init_vehicules.c
@@ -0,0 +1,111 @@
+#include "vehicule.h"
+#include <assert.h>
+
+vehicule* *tab;
+int max_id;
+
+void destroy(){
+    for (int i = max_id; i>0; i--){
+        free(tab[i]->modele);
+        free(tab[i]->marque);
+        free(tab[i]);
+    }
+    free(tab);
+}
+
+int
+main(
+	int argc,
+	char* *argv)
+{
+
+	 FILE* fp = fopen("../assets/ev-data.csv", "r");
+
+	int max_id = 0;
+
+    if (!fp)
+        printf("Importation du fichier ev-data impossible\n Initialisation iterompue\n\n");
+
+    else {
+        printf("Importation du fichier ev-data réussie!\n");
+        char buffer[1024];
+
+        int row = 0;
+        int column = 0;
+
+        tab = (vehicule* *)malloc(sizeof(vehicule));
+
+        while (fgets(buffer,
+                     1024, fp)) {
+            column = 0;
+            row++;
+
+            if (row == 1)
+                continue;
+
+            char* value = strtok(buffer, ";");
+
+            while (value) {
+    	    			vehicule *Vehicule = (vehicule *)malloc(sizeof(vehicule));
+    	    	        assert(Vehicule!=NULL);
+
+    	    	        char *modele_str;
+	    	    		modele_str = (char *)malloc((sizeof(char))*MAX_MODEL_NAME);
+	    	            assert(modele_str!=NULL);
+
+	    	            char *brand_str;
+						brand_str = (char *)malloc(sizeof(char)*MAX_BRAND_NAME);
+                        assert(brand_str!=NULL);
+
+	    	            tab = (vehicule* *)realloc(tab,sizeof(vehicule *)*max_id);
+	    	            assert(tab!=NULL);
+	    	            tab[max_id]=Vehicule;
+
+                switch (column){
+			        case 0:
+        				strcpy(modele_str, value);
+
+        				Vehicule->modele=modele_str;
+        				Vehicule->id = max_id;
+
+        				max_id++;
+            		break;
+
+                	case 1:
+	        			Vehicule->range = atoi(value);
+            		break;
+
+                    case 3:
+						Vehicule->top_speed = atoi(value);
+    				break;
+
+	        		case 4:
+						Vehicule->efficiency = atoi(value);
+				    break;
+
+			        case 5:
+						Vehicule->fast_charge = atoi(value);
+				    break;
+
+        			case 6:
+						strcpy(brand_str, value);
+
+						Vehicule->marque=brand_str;
+			    	break;
+		}
+                value = strtok(NULL, ";");
+                column++;
+            }
+
+        }
+        printf("Importation réussie!\n");
+        fclose(fp);
+    }
+
+    destroy();
+
+    	return 0;
+}
+
+
+
