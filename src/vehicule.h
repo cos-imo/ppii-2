74ed3d63c40c941e255e2c6f49b91a679915a965
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-05-19 18:11:52 +0200

    Ajout de l'initialisation des tableaux et réorganisation d'un fichier .h

--- src/vehicule.h
+++ src/vehicule.h
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define MAX_MODEL_NAME 100
+#define MAX_BRAND_NAME 100
+
 typedef struct _vehicule{
 	int id;				//A relier avec une table de hachage
 	char *modele;
-	double capacite;		//Pas de type spécifié sur le document -> faut-il garder double?
-	double consommation;
+	int range;
+	double acceleration;
+	int top_speed;
+	int efficiency;
+	int fast_charge;
+	char *marque;
 	int charge;
 	double temps_chargement;	//?
 	double latitude;		//Pourquoi faire, si l'on se déplace sur un graphe?
 	double longitude;		//Pareil
+
 } vehicule;
 
 typedef struct list_t {
