0cf030c204320813f95dca57a25f913cb1198024
eleve <Kieran.Daligaud@telecomnancy.eu>
2023-05-19 16:42:29 +0200

    main presque fini

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "string.h"
 
 
 #include "main.h"
+
+
+vehicule find_vehicle(char* model, vehicule* vehicle_table){
+	// On sait que le modèle est dans la liste
+    int size = 49;
+    for (int i=0; i<49; i++){
+        if (strcmp(vehicle_table[i].modele, model) == 0){
+            return vehicle_table[i];
+        }
+    }
+}
 
 
 int main(int argc, char** argv){
@@ -50,14 +62,16 @@ int main(int argc, char** argv){
 
         // Computing Dijkstra
 
+        // Initialize
         BorneElectrique* tableauBornes; // TODO Initialize
         vehicule* all_vehicles; // TODO Initialize
-
         char* list_models[50] = {"Tesla Model Y Long Range Dual Motor", "Tesla Model 3", "Tesla Model Y", "Tesla Model 3 Long Range Dual Motor", "BYD ATTO 3", "MG MG4 Electric 64 kWh", "Tesla Model S Plaid", "BMW i4 eDrive40", "BMW iX xDrive40", "Tesla Model Y Performance", "Kia Niro EV", "BMW iX1 xDrive30", "Mercedes EQS 450+", "Mercedes EQE 350+", "Hongqi E-HS9 99 kWh", "BYD HAN", "Audi Q8 e-tron 55 quattro", "Kia EV6 GT", "Tesla Model S Dual Motor", "Lexus RZ 450e", "Skoda Enyaq iV 80", "BMW i7 xDrive60", "Hyundai IONIQ 6 Long Range AWD", "Hyundai Kona Electric 64 kWh", "Audi Q4 e-tron 40", "Smart #1", "Nissan Ariya 87kWh", "BMW iX xDrive50", "Tesla Model 3 Performance", "Dacia Spring Electric 45", "BMW iX3", "BMW i4 M50", "Volkswagen ID. Buzz Pro", "BMW i4 eDrive35", "Audi e-tron GT RS", "Renault Megane E-Tech EV60 220hp", "MG ZS EV Long Range", "Fiat 500e Hatchback 42 kWh", "Mercedes EQC 400 4MATIC", "BYD TANG", "Renault Zoe ZE50 R110", "Kia EV6 Long Range 2WD", "Volkswagen ID.4 Pro", "Mercedes EQS 580 4MATIC", "Hyundai IONIQ 6 Long Range 2WD", "MG MG4 Electric 51 kWh", "Peugeot e-208", "Toyota bZ4X FWD", "Volkswagen ID.4 Pro Performance"};
-        vehicule vehicule = find_vehicle(vehicle_model, all_vehicles);
+        
+        // Parameters
+        // vehicule v = find_vehicle(vehicle_model, all_vehicles);
+        int range = (100 - not_below) * v.capacite / v.consommation;
+        // Trip *final_trip = dijkstra(tableauBornes, 10000, range, 0, 1);
 
-        int range = (1 - not_below) * vehicule.capacite / vehicule.consommation;
-        Trip *final_trip = dijkstra(tableauBornes, 10000, range, 0, 1);
     }
 
     else{
